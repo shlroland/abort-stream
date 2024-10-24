0cbd47613cb038974ab468c56e2c534b3452a2c1
shlroland <shlroland1995@gmail.com>
2024-10-24 22:40:00 +0800

    bun init

---
+++ client/App.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+import { hc } from 'hono/client'
+import type { App } from '../server'
+
+const hono = hc<App>('/api')
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    void (async () => {
+      const res = await hono.api.$get()
+      const decoder = new TextDecoder()
+      const reader = res.body!.getReader()
+      while (true) {
+        const { done, value } = await reader.read()
+        if (done) break
+        const text = decoder.decode(value, { stream: true })
+        console.log(text)
+      }
+      // from(res.body!).pipe(
+      //   map((chunk) => decoder.decode(chunk, { stream: true })),
+      // ).subscribe((text) => {
+      //   console.log(text)
+      // })
+    })()
+  }, [])
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.ts
@@ -0,0 +1,32 @@
+import { Hono } from 'hono'
+import { stream } from 'hono/streaming'
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+const app = new Hono()
+
+const api = new Hono().get('/', (c) => {
+  return stream(c, async (stream) => {
+    stream.onAbort(() => {
+      console.log('abort')
+    })
+
+    stream.write('Hello Hono!1')
+    await sleep(10000)
+    stream.write('Hello Hono!2')
+    await sleep(10000)
+    stream.write('Hello Hono!3')
+    await sleep(10000)
+    stream.write('Hello Hono!4')
+    await sleep(10000)
+    stream.write('Hello Hono!5')
+  })
+})
+
+const client = app.get('/', (c) => {
+  return c.text('Hello Hono!')
+}).route('/api', api)
+
+export type App = typeof client
+
+export default client
